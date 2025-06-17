e5ca20141a91d77f957b75c826c822decd1a67de
Sayato <amf.kano@gmail.com>
2025-06-18 02:04:20 +0800

    feat: Finishes Basic Statistics

--- src/components/StatisticsPane.tsx
+++ src/components/StatisticsPane.tsx
@@ -1,4 +1,6 @@
-import { M_to_FT } from "@/app/utils/math";
+import { M_to_FT, M_to_NM } from "@/app/utils/math";
+import { AirportsContext } from "@/contexts/airports";
+import { ConfigContext } from "@/contexts/config";
 import { StatisticsContext } from "@/contexts/statistics";
 import { JSX, useContext, useEffect, useState } from "react";
 
@@ -9,10 +11,17 @@ function StatisticsSection({
 }: {
     title: string;
     value: string | number | JSX.Element;
-    span?: number;
+    span?: 1 | 2 | 4 | 6 | 8;
 }) {
+    const spans = {
+        1: "col-span-1",
+        2: "col-span-2",
+        4: "col-span-4",
+        6: "col-span-6",
+        8: "col-span-8",
+    };
     return (
-        <div className={`col-span-${span ?? 1}`}>
+        <div className={spans[span ?? 1]}>
             <div className="bg-slate-300 p-2 rounded-sm">
                 <p className="text-xs text-slate-800">{title}</p>
                 <span className="text-md text-black">{value}</span>
@@ -23,25 +32,103 @@ function StatisticsSection({
 
 export function StatisticsPane() {
     const statistics = useContext(StatisticsContext);
+    const airports = useContext(AirportsContext);
+    const { config } = useContext(ConfigContext);
+    const { referenceTrack, arrivalAirport } = config ?? {};
     console.log("StatisticsPane statistics:", statistics);
     return (
-        <div className="bg-slate-600 text-white p-4 pt-8 grid grid-cols-8 gap-2 -mb-2 absolute w-full h-full z-1000">
-            <StatisticsSection
-                title="Speed"
-                value={`${statistics.speed?.toFixed(1)} kts`}
-                span={2}
-            />
-            <StatisticsSection
-                title="V/S"
-                value={`${statistics.verticalSpeed?.toFixed(1)} fpm`}
-                span={2}
-            />
-            <StatisticsSection
-                title="Altitude"
-                value={`${M_to_FT(statistics.position?.coords.altitude ?? 0).toFixed(1)} ft`}
-                span={2}
-            />
-            <StatisticsSection title="UTC Time" value={<Clock />} span={2} />
+        <div className="bg-slate-600 text-white p-4 pt-8 -mb-2 absolute w-full h-full z-1000">
+            <div className="grid grid-cols-8 gap-2">
+                <StatisticsSection
+                    title="Speed"
+                    value={`${statistics.speed?.toFixed(1)} kts`}
+                    span={2}
+                />
+                <StatisticsSection
+                    title="V/S"
+                    value={`${statistics.verticalSpeed?.toFixed(1)} fpm`}
+                    span={2}
+                />
+                <StatisticsSection
+                    title="Altitude"
+                    value={`${M_to_FT(statistics.position?.coords.altitude ?? 0).toFixed(1)} ft`}
+                    span={2}
+                />
+                <StatisticsSection
+                    title="UTC Time"
+                    value={<Clock />}
+                    span={2}
+                />
+                <StatisticsSection
+                    title="Flight Status"
+                    value={
+                        <>
+                            {statistics.eta ? (
+                                `ETA $
+                            {statistics.eta.toLocaleTimeString()}`
+                            ) : !referenceTrack?.flightPath.trackPoints
+                                  .length ? (
+                                <span className="text-slate-600 text-sm">
+                                    Load previous flight track log for ETA
+                                </span>
+                            ) : (
+                                <span className="text-slate-600 text-sm">
+                                    ETA unavailable during takeoff/landing stage
+                                </span>
+                            )}
+                            <br />
+                            {statistics.distanceToDestination ? (
+                                `${M_to_NM(
+                                    statistics.distanceToDestination,
+                                ).toFixed(1)} NM to ${
+                                    airports.find(
+                                        (airport) =>
+                                            airport.key === arrivalAirport,
+                                    )?.name ?? arrivalAirport
+                                }`
+                            ) : config?.arrivalAirport ? (
+                                <span className="text-slate-600 text-sm">
+                                    Could not calculate distance to destination
+                                </span>
+                            ) : (
+                                <span className="text-slate-600 text-sm">
+                                    Set arrival airport in configurations to
+                                    show distance
+                                </span>
+                            )}
+                        </>
+                    }
+                    span={8}
+                />
+                <StatisticsSection
+                    title="Position"
+                    value={
+                        statistics.position ? (
+                            `${statistics.position.coords.latitude.toFixed(
+                                4,
+                            )}, ${statistics.position.coords.longitude.toFixed(
+                                4,
+                            )}`
+                        ) : (
+                            <span className="text-slate-600 text-sm">
+                                Waiting for GPS signal...
+                            </span>
+                        )
+                    }
+                    span={4}
+                />
+                <StatisticsSection
+                    title="GPS Last Updated"
+                    value={
+                        statistics.position
+                            ? new Date(
+                                  statistics.position.timestamp,
+                              ).toLocaleTimeString()
+                            : "N/A"
+                    }
+                    span={4}
+                />
+            </div>
         </div>
     );
 }
--- src/contexts/statistics.tsx
+++ src/contexts/statistics.tsx
@@ -1,7 +1,14 @@
 import { getNearestAirport } from "@/app/utils/airportsHelper";
 import { createContext, useContext, useEffect, useState } from "react";
 import { AirportsContext } from "./airports";
-import { calculateMeanSpeed, calculateMeanVertSpeed } from "@/app/utils/math";
+import {
+    calculateHaversineDistance,
+    calculateMeanSpeed,
+    calculateMeanVertSpeed,
+    estimateTimeOfArrival,
+} from "@/app/utils/math";
+import { ConfigContext } from "./config";
+import FlightPathStore from "@/app/utils/flightPathStore";
 
 type Statistics = {
     position?: GeolocationPosition;
@@ -10,40 +17,52 @@ type Statistics = {
     speed?: number;
     verticalSpeed?: number;
     gpsErrored: boolean;
+    eta: Date | null;
+    flightPath: FlightPath;
+    distanceToDestination?: number | null;
 };
 
-export const StatisticsContext = createContext<Statistics>({
+const flightPathStore = new FlightPathStore();
+
+const defaultStatistics: Statistics = {
     position: undefined,
     recentPositions: [],
     nearestAirport: null,
     speed: undefined,
     verticalSpeed: undefined,
     gpsErrored: false,
-});
+    eta: null,
+    flightPath: {
+        trackPoints: [],
+    },
+    distanceToDestination: null,
+};
+
+export const StatisticsContext = createContext<Statistics>(defaultStatistics);
 
 export const StatisticsProvider = ({
     children,
 }: {
     children: React.ReactNode;
 }) => {
-    const [statistics, setStatistics] = useState<Statistics>({
-        position: undefined,
-        recentPositions: [],
-        nearestAirport: null,
-        speed: undefined,
-        verticalSpeed: undefined,
-        gpsErrored: false,
-    });
+    const [statistics, setStatistics] = useState<Statistics>(defaultStatistics);
     const [position, setPosition] = useState<GeolocationPosition | undefined>(
         undefined,
     );
     const [recentPositions, setRecentPositions] = useState<
         GeolocationPosition[]
     >([]);
+    const [flightPath, setFlightPath] = useState<FlightPath>({
+        trackPoints: [],
+    });
 
     const airports = useContext(AirportsContext);
+    const { config } = useContext(ConfigContext);
+    const { referenceTrack, arrivalAirport } = config ?? {};
 
     useEffect(() => {
+        flightPathStore.setStorage(localStorage);
+
         if ("geolocation" in navigator) {
             console.log("Starting GPS watchPosition...");
             const watcher = navigator.geolocation.watchPosition(
@@ -74,20 +93,20 @@ export const StatisticsProvider = ({
                 const updated = [...prev, position].slice(-10);
                 return updated;
             });
+            flightPathStore.addTrackPoint({
+                lat: position.coords.latitude,
+                lon: position.coords.longitude,
+                alt: position.coords.altitude ?? 0,
+                timestamp: position.timestamp,
+            });
+            setFlightPath(flightPathStore.getFlightPath());
         }
     }, [position]);
 
     useEffect(() => {
         if (position) {
             const recent = [...recentPositions, position].slice(-10);
-            console.log("GPS position updated:", {
-                position,
-                recentPositions: recent,
-                nearestAirport: getNearestAirport(position, airports),
-                speed: calculateMeanSpeed(recent),
-                verticalSpeed: calculateMeanVertSpeed(recent),
-                gpsErrored: false,
-            });
+
             setStatistics({
                 position,
                 recentPositions: recent,
@@ -95,9 +114,42 @@ export const StatisticsProvider = ({
                 speed: calculateMeanSpeed(recent),
                 verticalSpeed: calculateMeanVertSpeed(recent),
                 gpsErrored: false,
+                eta: referenceTrack?.flightPath.trackPoints.length
+                    ? estimateTimeOfArrival(
+                          flightPath.trackPoints,
+                          referenceTrack.flightPath.trackPoints,
+                      )
+                    : null,
+                flightPath: {
+                    trackPoints: recent.map((pos) => ({
+                        lat: pos.coords.latitude,
+                        lon: pos.coords.longitude,
+                        alt: pos.coords.altitude ?? 0,
+                        timestamp: pos.timestamp,
+                    })),
+                },
+                distanceToDestination: config?.arrivalAirport
+                    ? calculateHaversineDistance(
+                          position.coords.latitude,
+                          position.coords.longitude,
+                          airports.find(
+                              (airport) => airport.key === arrivalAirport,
+                          )?.lat ?? 0,
+                          airports.find(
+                              (airport) => airport.key === arrivalAirport,
+                          )?.lon ?? 0,
+                      )
+                    : null,
             });
         }
-    }, [position, airports, recentPositions]);
+    }, [
+        position,
+        airports,
+        recentPositions,
+        flightPath,
+        referenceTrack,
+        arrivalAirport,
+    ]);
 
     return (
         <StatisticsContext.Provider value={statistics}>
