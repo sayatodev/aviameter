63981510028234da73dc1863f1a1194959f9fd3a
Sayato <amf.kano@gmail.com>
2025-06-18 12:49:24 +0800

    fix: Update Map Component - Fixes #2

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,29 +1,18 @@
 "use client";
-import { useContext, useEffect, useMemo, useState } from "react";
+import { useContext, useEffect, useMemo } from "react";
 import dynamic from "next/dynamic";
-import {
-    airportIsSized,
-    airportIsValid,
-} from "../../utils/airportsHelper";
-
-import FlightPathStore from "../../utils/flightPathStore";
-import ConfigStore from "../../utils/configStore";
 import { ConfigContext } from "@/contexts/config";
 import { AirportsContext } from "@/contexts/airports";
-
-const _stores = {
-    config: new ConfigStore(),
-    flightPath: new FlightPathStore(),
-};
+import { StatisticsContext } from "@/contexts/statistics";
+import { airportIsSized } from "@/utils/airportsHelper";
 
 export default function Main() {
     /* Config */
     const configCtx = useContext(ConfigContext);
     const { config } = configCtx;
 
-    const [flightPath, _setFlightPath] = useState<FlightPath>({
-        trackPoints: [],
-    });
+    /* Flight Path */
+    const { flightPath } = useContext(StatisticsContext);
 
     /* Airports */
     const airportsData = useContext(AirportsContext);
@@ -48,30 +37,13 @@ export default function Main() {
         [],
     );
 
-    /* Updating flight path */
-    // useEffect(() => {
-    //     if (!position || !running) return;
-    //     stores.flightPath.addTrackPoint({
-    //         lat: position.coords.latitude,
-    //         lon: position.coords.longitude,
-    //         alt: position.coords.altitude ?? 0,
-    //         timestamp: position.timestamp,
-    //     });
-    //     setFlightPath(stores.flightPath.getFlightPath());
-    // }, [position, running]);
-
-    /* Rendering */
     return (
         <div className="overflow-x-hidden flex flex-col gap-2 justify-center align-middle w-full h-full">
             <div className="flex flex-col gap-2 w-fit mx-auto min-w-[20vw] md:px-10 px-3">
                 <div className="w-[100vw] -mx-[50px] max-w-[768px] h-[90vh] overflow-hidden">
                     <Map
                         config={config}
-                        airports={
-                            airportsData
-                                ?.filter(airportIsValid)
-                                .filter(airportIsSized) ?? []
-                        }
+                        airports={airportsData.filter(airportIsSized)}
                         flightPath={flightPath}
                     />
                 </div>
